2b87a1f6a9c96713d6249fa1d756c43d92a264ae
Christofm <github@burningwheels.com>
2022-03-11 09:09:39 +0100

    Second commit ;)

--- main.py
+++ main.py
@@ -3,17 +3,25 @@
 from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from libby.remote import udpRemote
+
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
-@app.route('/hello.html')
-def hello():
+@app.route('/garage')
+def garage():
+    return render_template('index.html')
+
+@app.route('/ampi')
+def ampi():
+    return render_template('ampi.html')
+    
+
+@app.route('/')
+def index():
     nachricht = "Hallo Welt!"
-    return render_template('hello.html', nachricht=nachricht)
+    return render_template('index.html', nachricht=nachricht)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
