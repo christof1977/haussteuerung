7756bb5c9736f8bbef8339b175e229adefed65c0
Christofm <github@burningwheels.com>
2022-03-11 08:26:12 +0100

    Initial Checkin

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/hello.html')
+def hello():
+    nachricht = "Hallo Welt!"
+    return render_template('hello.html', nachricht=nachricht)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
